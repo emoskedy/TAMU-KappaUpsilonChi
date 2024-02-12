3318801b100a31e9e62b20bb08a544a95d8e02c5
root <root@localhost>
2024-02-12 16:57:13 +0000

    addeddb migrate admins and modified admins controller now successfully logining in and signing out

--- app/controllers/admins/omniauth_callbacks_controller.rb
+++ app/controllers/admins/omniauth_callbacks_controller.rb
@@ -1,4 +1,4 @@
-class admins::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+class Admins::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
     admin = Admin.from_google(**from_google_params)
 
