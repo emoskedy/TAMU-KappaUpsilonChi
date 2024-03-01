3f8969c070bba77ff0bcc7de44e3e378f5e77de5
JerryTran <tommy.trananh@gmail.com>
2024-03-01 16:46:15 -0600

    modified addrolestoadmin to prevent duplicate column

--- db/migrate/20240301205906_add_roles_to_admins.rb
+++ db/migrate/20240301205906_add_roles_to_admins.rb
@@ -1,6 +1,11 @@
 class AddRolesToAdmins < ActiveRecord::Migration[7.0]
   def change
-    add_column :admins, :is_officer, :boolean, default: false
-    add_column :admins, :is_admin, :boolean, default: false
+    unless column_exists?(:admins, :is_officer)
+      add_column :admins, :is_officer, :boolean, default: false
+    end
+    
+    unless column_exists?(:admins, :is_admin)
+      add_column :admins, :is_admin, :boolean, default: false
+    end
   end
 end
