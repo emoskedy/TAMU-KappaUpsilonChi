37a8e49a60d8fcb33ed9d553ed9573acfb34be58
root <root@localhost>
2024-03-01 18:16:17 +0000

    only admins and officers can access subaccount and perform CRUD

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_01_173304) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_01_174430) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admin_emails", force: :cascade do |t|
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admin_emails_on_email"
+  end
 
   create_table "admins", force: :cascade do |t|
     t.string "email", null: false
@@ -21,7 +28,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_01_173304) do
     t.string "avatar_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "role", default: "member"
+    t.boolean "is_officer", default: false
+    t.boolean "is_admin", default: false
     t.index ["email"], name: "index_admins_on_email", unique: true
   end
 
@@ -37,31 +45,29 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_01_173304) do
     t.text "payable_address"
     t.string "payment_method"
     t.string "role"
-    t.bigint "sub_account_id", null: false
+    t.bigint "sub_account_id"
     t.text "approval_status"
     t.text "comments"
     t.decimal "dollar_amount", precision: 10, scale: 2
-    t.decimal "travel", default: "0.0"
-    t.decimal "food", default: "0.0"
-    t.decimal "office_supplies", default: "0.0"
-    t.decimal "utilities", default: "0.0"
-    t.decimal "membership", default: "0.0"
-    t.decimal "clothing", default: "0.0"
-    t.decimal "rent", default: "0.0"
-    t.decimal "other_expenses", default: "0.0"
-    t.decimal "items_for_resale", default: "0.0"
+    t.decimal "travel", precision: 10, scale: 2, default: "0.0"
+    t.decimal "food", precision: 10, scale: 2, default: "0.0"
+    t.decimal "office_supplies", precision: 10, scale: 2, default: "0.0"
+    t.decimal "utilities", precision: 10, scale: 2, default: "0.0"
+    t.decimal "membership", precision: 10, scale: 2, default: "0.0"
+    t.decimal "clothing", precision: 10, scale: 2, default: "0.0"
+    t.decimal "rent", precision: 10, scale: 2, default: "0.0"
+    t.decimal "other_expenses", precision: 10, scale: 2, default: "0.0"
+    t.decimal "items_for_resale", precision: 10, scale: 2, default: "0.0"
     t.decimal "services_and_other_income", precision: 10, scale: 2, default: "0.0"
     t.bigint "admin_id", null: false
     t.index ["admin_id"], name: "index_checks_on_admin_id"
     t.index ["sub_account_id"], name: "index_checks_on_sub_account_id"
   end
 
-  create_table "notes", force: :cascade do |t|
-    t.string "picture"
-    t.bigint "form_id"
+  create_table "officer_emails", force: :cascade do |t|
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
   end
 
   create_table "people", force: :cascade do |t|
