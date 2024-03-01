cc2b3599eaec6f4bb029c1bd99feb80c895300f6
emoskedy <nghilam@tamu.edu>
2024-03-01 14:33:38 -0600

    figured out why seed data doesnt work

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,18 +7,17 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 admin_emails = [
-  { email: 'nghilam@tamu.edu', full_name: 'Tran Lam', is_officer: false, is_admin: true },
-  { email: 'mikekha109@tamu.edu', full_name: 'Michael Kha', is_officer: false, is_admin: true },
-  { email: 'alejandro.muller@tamu.edu', full_name: 'Alex Muller', is_officer: false, is_admin: true },
-  { email: 'jerrytran@tamu.edu', full_name: 'Jerry Tran', is_officer: false, is_admin: true},
+  { email: 'nghilam@tamu.edu', full_name: 'Tran Lam', role: "admin" },
+  { email: 'mikekha109@tamu.edu', full_name: 'Michael Kha', role: "admin" },
+  { email: 'alejandro.muller@tamu.edu', full_name: 'Alex Muller', role: "admin" },
+  { email: 'jerrytran@tamu.edu', full_name: 'Jerry Tran', role: "admin"},
   # Add more email accounts as needed
 ]
 
 admin_emails.each do |data|
   Admin.find_or_create_by(email: data[:email]) do |admin|
     admin.full_name = data[:full_name]
-    admin.is_officer = data[:is_officer]
-    admin.is_admin = data[:is_admin]
+    admin.role = data[:role]
     admin.save!
   end
 end
