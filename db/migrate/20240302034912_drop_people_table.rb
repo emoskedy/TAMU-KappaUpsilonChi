4c3fd9760786d75339c533275b7e37d8d75535e9
root <root@localhost>
2024-03-04 20:20:51 +0000

    fixed heroku issues of migration drop_people and require dotenv in config/application.rb

--- db/migrate/20240302034912_drop_people_table.rb
+++
@@ -1,9 +0,0 @@
-class DropPeopleTable < ActiveRecord::Migration[6.0]
-  def up
-    drop_table :people
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -14,13 +14,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_04_015246) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "admin_emails", force: :cascade do |t|
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_admin_emails_on_email"
-  end
-
   create_table "admins", force: :cascade do |t|
     t.string "email", null: false
     t.string "full_name"
@@ -69,12 +62,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_04_015246) do
     t.bigint "form_id"
     t.string "name"
     t.string "avatar_url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "officer_emails", force: :cascade do |t|
-    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
